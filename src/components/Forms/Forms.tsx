b5ffbca902669bbd4904af9738365b80eb0ada8e
Bappa Saha <bappasaha161@gmail.com>
2023-10-02 22:45:12 +0600

    6-create hook from provider

---
+++ src/components/Forms/Forms.tsx
@@ -0,0 +1,43 @@
+import { ReactElement, ReactNode } from "react";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+
+
+type FormConfig={
+    defaultValues?:Record<string,any>;
+};
+
+type FormProps={
+    children?:ReactElement |ReactNode;
+    submitHandler:SubmitHandler<any>;
+} & FormConfig;
+
+
+const Forms = ({children,submitHandler,defaultValues}:FormProps) => {
+
+    const formConfig:FormConfig={};
+
+    //check js truthy value. not working on falsy value
+    if(!!defaultValues) formConfig["defaultValues"]=defaultValues;
+
+    const methods=useForm<FormProps>(formConfig);
+    console.log(methods);
+
+    const {handleSubmit,reset}=methods;
+
+    const onSubmit = (data:any) => {
+        console.log(data);
+        submitHandler(data);
+        reset();
+    };
+
+    return (
+        <FormProvider {...methods}>
+          
+          <form onSubmit={handleSubmit(onSubmit)}>
+           {children}
+          </form>
+        </FormProvider>
+      )
+};
+
+export default Forms;
