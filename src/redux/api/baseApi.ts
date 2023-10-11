46fc866c85dbb8970a12b883e250dd4032454553
Bappa Saha <bappasaha161@gmail.com>
2023-10-12 00:24:24 +0600

    v2-Base api creation done

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,17 @@
+import { axiosBaseQuery } from '@/helpers/axios/axiosBaseQuery'
+import { getBaseUrl } from '@/helpers/config/envConfig'
+import { createApi } from '@reduxjs/toolkit/query/react'
+
+// Define a service using a base URL and expected endpoints
+//here we use custom axios
+export const baseApi = createApi({
+  reducerPath: 'api',
+//   baseQuery: fetchBaseQuery({ baseUrl:getBaseUrl() }),
+  baseQuery: axiosBaseQuery({ baseUrl:getBaseUrl() }),
+  endpoints: (builder) => ({
+   
+  }),
+  //for caching data
+  tagTypes:['user']
+})
+
