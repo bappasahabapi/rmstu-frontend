ecae0b2f0d90a8ebe2ea1d8697620829eb329625
Bappa Saha <bappasaha161@gmail.com>
2023-12-08 12:18:24 +0600

    set dynamic data to department

--- src/app/(withlayout)/super_admin/admin/create/page.tsx
+++ src/app/(withlayout)/super_admin/admin/create/page.tsx
@@ -6,13 +6,33 @@ import FormSelectField from '@/components/Forms/FormSelectField';
 import FormTextArea from '@/components/Forms/FormTextArea';
 import UMBreadCrumb from '@/components/ui/UMBreadCrumb';
 import UploadImage from '@/components/ui/UploadImage';
-import { bloodGroupOptions, departmentOptions, genderOptions } from '@/constants/global';
+import {
+    bloodGroupOptions,
+    // departmentOptions,
+    genderOptions
+} from '@/constants/global';
+import { useDepartmentsQuery } from '@/redux/api/departmentApi';
 import { adminSchema } from '@/schemas/admin';
+import { IDepartment } from '@/types';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Button, Col, Row } from 'antd';
 import React from 'react';
 
 const CreateAdminPage = () => {
+
+    const {data,isLoading} = useDepartmentsQuery({ limit: 100, page: 1 });
+    console.log(data);
+    //@ts-ignore
+    const departments:IDepartment[] = data?.departments;
+    const departmentOptions = departments && departments.map((department)=>{
+        return{
+            label:department?.title,
+            value:department?.id
+        };
+    })
+
+
+
     const onSubmit = async (data: any) => {
         try {
             console.log(data)
